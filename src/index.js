911b0d5f183a0c69ba46cbcb5134d5430e9705f9
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-12-11 20:55:25 +0100

    frst test

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { LitElement, html, css } from "lit";
+
+customElements.define("video-element", class VideoElement extends LitElement {
+
+    static get styles() {
+        return css`
+            :host {
+                
+            }
+
+            video {
+                width: 100%;
+                height: 100%;
+            }
+        `;
+    }
+
+    constructor() {
+        super();
+
+        this.videoEle = document.createElement("video");
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.videoEle.src = "./assets/brb_1.mov";
+        this.videoEle.muted = true;
+        this.videoEle.oncanplay = () => {
+            this.videoEle.play();
+        }
+    }
+
+    render() {
+        return html`
+            ${this.videoEle}
+        `;
+    }
+
+});
