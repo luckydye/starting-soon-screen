67496f045c5933112aaeba8cceb6e1607c7af286
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-12-19 21:35:25 +0100

    ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,118 @@ import { LitElement, html, css } from "lit";
 
 // TODO: Make a file selection overlay on mouse over
 
+function deserialize(lines, startKeyframesIndicator, endKeyframesIndicator) {
+    let seq = false;
+
+    const deserialized = [];
+
+    for(let line of lines) {
+        if(line == "") {
+            continue;
+        }
+
+        if (line == startKeyframesIndicator) {
+            seq = true;
+        }
+        if (line == endKeyframesIndicator) {
+            seq = false;
+        }
+
+        if(seq) {
+            const parts = line.split("\t");
+            let i = 0;
+            for(let part of parts) {
+                if(!deserialized[i]) deserialized[i] = [];
+                deserialized[i].push(part);
+                i++;
+            }
+        }
+    }
+
+    return {
+        "frame": deserialized[1].slice(1).map(d => +d),
+        "x": deserialized[2].slice(1).map(d => +d),
+        "y": deserialized[3].slice(1).map(d => +d),
+    }
+}
+
+function getKeyValue(lines, key) {
+    for(let line of lines) {
+        if(line.match(key)) {
+            return line.split("\t")[2];
+        }
+    }
+}
+
+async function getKeyframes() {
+    const raw_data = await fetch(`../assets/${window.movieFiles[0]}.keyframes.txt`).then(res => {
+        if(res.status !== 200) {
+            throw new Error("Failed to fetch keyframe data.");
+        }
+        return res.text();
+    });
+    const lines = raw_data.split(/\n|\r/g);
+
+    const startKeyframesIndicator = "	Frame	X pixels	Y pixels	";
+    const endKeyframesIndicator = "End of Keyframe Data";
+
+    const data = deserialize(lines, startKeyframesIndicator, endKeyframesIndicator);
+
+    data.fps = +getKeyValue(lines, "Units Per Second");
+    data.source_width = +getKeyValue(lines, "Source Width");
+    data.source_height = +getKeyValue(lines, "Source Height");
+    data.length = data.frame.length;
+
+    return data;
+}
+
+window.addEventListener("DOMContentLoaded", async e => {
+    const keyframes = await getKeyframes();
+    console.log(keyframes);
+    console.log('loaded');
+
+    const iframe = document.querySelector("iframe");
+    const video = document.querySelector("video-element").videoEle;
+
+    pin(iframe, keyframes, [-605, -484.5], video, 0.9);
+});
+
+function pin(target, keyframes, offset = [0, 0], sync, scale = 1) {
+
+    let lastFrame = 0;
+    let acc = 0;
+    let frame = 0;
+
+    const fps = keyframes.fps;
+
+    const loop = (ms = 0) => {
+
+        const delta = ms - lastFrame;
+        acc += delta;
+
+        while(acc >= 1000 / fps) {
+            frame++;
+            acc -= 1000 / fps;
+
+            if(frame > keyframes.length) {
+                frame = 0;
+            }
+
+            frame = Math.round(sync.currentTime * fps);
+
+            const x = keyframes.x[frame] + offset[0];
+            const y = keyframes.y[frame] + offset[1];
+
+            target.style.transform = `translateZ(0) translate(${x * scale}px, ${y * scale}px)`;
+        }
+
+        lastFrame = ms;
+        requestAnimationFrame(loop);
+    };
+
+    loop();
+}
+
 customElements.define("video-element", class VideoElement extends LitElement {
 
     static get styles() {
@@ -26,7 +138,7 @@ customElements.define("video-element", class VideoElement extends LitElement {
     connectedCallback() {
         super.connectedCallback();
 
-        this.videoEle.src = window.movieFiles[0];
+        this.videoEle.src = window.movieFiles[0] + ".mov";
         this.videoEle.muted = true;
         this.videoEle.loop = true;
         this.videoEle.oncanplay = () => {
